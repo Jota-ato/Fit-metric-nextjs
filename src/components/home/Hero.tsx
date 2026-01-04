cdaaf75eaeb6a61459cd2d2f940e4f4a8a898530
Jota-ato <julioc-01@outlook.com>
2026-01-03 22:51:09 -0600

    refactor: remove images from hero section.

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,14 +1,10 @@
 
-import Image from "next/image"
-import heroDesktop from "../../assets/images/hero-banner-desktop.webp"
-import heroMobile from "../../assets/images/hero-banner-mobile.webp"
-
 export default function Hero() {
     return (
         <>
-            <section className="w-full">
-                <div className="container flex flex-col md:flex-row gap-8 items-center">
-                    <div className="space-y-8" style={{ flexBasis: "40%" }}>
+            <section className="w-full lg:min-h-screen">
+                <div className="container flex flex-col lg:flex-row gap-8 items-center">
+                    <div className="space-y-8" style={{ flexBasis: "50%" }}>
                         <h2 className="text-6xl md:text-7xl font-black leading-14 md:leading-19">Eres único. El camino hacia tus objetivos también lo es</h2>
                         <p className="text-2xl md:text-4xl">Adelgazar no siempre es fácil. Encontrar lo que funciona, <span className="text-secondary font-bold">mantener la motivación y ver avances reales puede ser todo un reto.</span></p>
                         <p className="text-2xl md:text-4xl"><span className="text-primary font-bold">Lo entendemos.</span> Por eso fitmetric te da herramientas y apoyo hechos a tu medida.</p>
@@ -17,22 +13,6 @@ export default function Hero() {
                         >
                             Comienza tu transformación ahora
                         </button>
-                    </div>
-                    <div style={{ flexBasis: "60%" }}>
-                        <Image
-                            src={heroDesktop}
-                            alt="Hero"
-                            width={1600}
-                            height={900}
-                            className="animate-fade-in-up animate-duration-1000 w-full object-cover hidden md:block"
-                        />
-                        <Image
-                            src={heroMobile}
-                            alt="Hero"
-                            width={600}
-                            height={900}
-                            className="animate-fade-in-up animate-duration-1000 w-full h-full object-cover block md:hidden"
-                        />
                     </div>
                 </div>
             </section>
